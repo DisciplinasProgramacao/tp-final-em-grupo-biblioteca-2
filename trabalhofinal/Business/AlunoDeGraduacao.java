b11d96fab3d7e9864829b32575834cf7ed18ca2f
jonnyver <69541959+jonnyver@users.noreply.github.com>
2022-06-27 16:34:59 -0300

    Create AlunoDeGraduacao.java

---
+++ trabalhofinal/Business/AlunoDeGraduacao.java
@@ -0,0 +1,42 @@
+package Business;
+
+public class AlunoDeGraduacao extends Usuario {
+	protected boolean usuarioSuspenso;
+	protected int diasSuspenso;
+	public AlunoDeGraduacao(String nome,Data dataDeDevolucao) {
+		super(nome, dataDeDevolucao);
+		this.usuarioSuspenso = false;
+		
+		
+	}
+
+	@Override
+	public boolean atrasado(Data dataAtual, Data devolucao) {
+		if (dataAtual.maisFutura(devolucao)) {
+			System.out.println("entrou");
+			System.out.println("devolucao"+devolucao.dataFormatada());
+			System.out.println("atual"+dataAtual.dataFormatada());
+			this.diasSuspenso += calculaSuspensao(dataAtual, devolucao);
+			this.usuarioSuspenso = true;
+			return true;
+		}
+		return false;
+	}
+
+	private int calculaSuspensao(Data dataAtual, Data devolucao) {
+		int aux = dataAtual.dia + (dataAtual.mes * 31) + (dataAtual.ano * 372);
+		int aux2 = devolucao.dia + (dataAtual.mes * 31) + (dataAtual.ano * 372);
+
+		return (aux - aux2) * 2;
+	}
+
+   @Override
+   public void addEmprestimo(LivroFisico livro) {
+	   
+	   this.dataDeDevolucao = this.dataDeDevolucao.acrescentaDias(7);
+	   livros.add(livro);
+
+	   
+   }
+
+}
